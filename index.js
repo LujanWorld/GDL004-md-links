863d66e756646c66bab7158b94ccff32029260ef
Maria Lujan Navarro <lujannavarro26@gmail.com>
2020-02-10 15:52:34 -0600

    Setup project

---
+++ index.js
@@ -0,0 +1,8 @@
+// Write a function which takes argument path to markdown file and returns all the links that file.
+function getLinks(path) {
+  let links = [];
+
+  return links;
+}
+
+let links = getLinks('test/data/simple.md');
