31929813fe9c6a7e3afa6109a3f9311e0435d845
Maria Lujan Navarro <lujannavarro26@gmail.com>
2020-02-24 07:52:04 -0600

    getting links and name

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@ const mdLinks = require('./index.js');
 let input = {};
 var myArgs = process.argv.slice(2); //remove 2 from the front
 
-console.log('myArgs: ', myArgs);
+// console.log('myArgs: ', myArgs);
 
 myArgs.forEach(function(name) {
   switch (name) {
@@ -23,10 +23,41 @@ myArgs.forEach(function(name) {
       }
   }
 });
-// Options
-console.log(input);
-// Call the mdLinks.
 
+// Options
+//console.log(input);
+
+//Call the mdLinks
+async function main() {
+  let links;
+  try {
+    links = await mdLinks(input.path, { validate: input.validate });
+    links.forEach(link => {
+      if (input.validate) {
+        console.log(
+          link.path,
+          link.href,
+          link.text,
+          link.ok ? 'Ok' : 'Fail', // alternative if
+          link.status
+        );
+      } else {
+        console.log(link.path, link.href, link.text);
+      }
+
+      // let successCounter = 0;
+      // failCounter = 0;
+      // var theFilter = link.filter(function(broken) {
+      //   //This tests if student.grade is greater than or equal to 90. It returns the "student" object if this conditional is met.
+      //   return broken.link === false;
+      // });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
 // If you have stats = true
 
 // Calculate stats.
